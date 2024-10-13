b85fb8feec67e78313a5cb95665c19f8baf9df0f
Troy Boswell <troy.boswell@dewc.com>
2024-10-13 11:47:09 +0000

    feat: added set impairment dto

---
+++ app/dtos/set_impairment_dtos.py
@@ -0,0 +1,7 @@
+from pydantic import BaseModel, Field
+
+
+# what will happen is the bearer and environment settings will be combined to create a complete hbt and netem settings
+class SetImpairment(BaseModel):
+    bearer_id: int = Field(..., description="Bearer Profile Id")
+    environment_id: int = Field(..., description="Environment Profile Id")
