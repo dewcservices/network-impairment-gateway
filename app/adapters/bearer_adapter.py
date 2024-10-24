fc946a27e4d40a9270c18d2fdc5c81f608dbfe89
tnbozman <tnbozman@gmail.com>
2024-10-24 14:59:39 +1030

    feat: bearer adapter implemented and tested

--- app/adapters/bearer_adapter.py
+++ app/adapters/bearer_adapter.py
@@ -1,8 +1,9 @@
-from typing import List
+from typing import Dict, List
 
 from app.adapters.dto_to_model_util import DtoToModelUtil
 from app.adapters.hbt_adapter import HBTAdapter
 from app.adapters.netem_adapter import NetemAdapter
+from app.constants import LinkTypes
 from app.dtos.bearer_dtos import (
     BearerDetailsDTO,
     BearerDTO,
@@ -26,11 +27,6 @@ class BearerAdapter:
         return [BearerAdapter.BearerToBearerDetailsDTO(bearer) for bearer in bearers]
 
     @staticmethod
-    def BearerToBearerDTO(bearer: Bearer) -> BearerDTO:
-        dict_dto = DtoToModelUtil.map_object_to_dto(bearer, BearerDTO, {"links": {}})
-        return BearerDTO.model_validate(dict_dto)
-
-    @staticmethod
     def BearerDTOToBearer(dto: BearerDTO) -> Bearer:
         # Convert Pydantic BearerDTO to SQLAlchemy Bearer using .dict() and **kwargs
         bearer_dict = dto.model_dump()
@@ -47,32 +43,54 @@ class BearerAdapter:
         return Bearer(**valid_keys)
 
     @staticmethod
-    def BearerLinkToBearerLinkDTO(bearer_link: BearerLink) -> BearerLinkDTO:
-        dto = BearerLinkDTO()
-        dto.netem = BearerAdapter.BearerNetemToBearerNetemDTO(
-            bearer_link.bearer_link_netem
-        )
-        dto.hbt = BearerAdapter.BearerNetemToBearerHtbDTO(bearer_link.bearer_link_hbt)
+    def BearerToBearerDTO(bearer: Bearer) -> BearerDTO:
+        dict_dto = DtoToModelUtil.map_object_to_dto(bearer, BearerDTO, {"links": {}})
+        dto = BearerDTO.model_validate(dict_dto)
 
-        # nest to a full bearer link with all children
+        for link in bearer.bearer_links:
+            if isinstance(link, BearerLink):
+                print(
+                    f"link_type_id: {link.link_type_id}, type: {type(link.link_type_id)}"
+                )
+                if link.link_type_id == int(LinkTypes.UPLINK.value):
+                    dto.links[LinkTypes.UPLINK.name] = (
+                        BearerAdapter.BearerLinkToBearerLinkDTO(link)
+                    )
+                elif link.link_type_id == int(LinkTypes.DOWNLINK.value):
+                    dto.links[LinkTypes.DOWNLINK.name] = (
+                        BearerAdapter.BearerLinkToBearerLinkDTO(link)
+                    )
         return dto
+
+    @staticmethod
+    def LinkInLinks(key: str, links: Dict[str, BearerLink]) -> BearerLink:
+        if key in links:
+            return links[key]
+
+        raise ValueError(f"Link {key} is missing from Bearer details")
+
+    @staticmethod
+    def BearerLinkToBearerLinkDTO(bearer_link: BearerLink) -> BearerLinkDTO:
+        netem = BearerAdapter.BearerNetemToBearerNetemDTO(bearer_link.bearer_link_netem)
+        hbt = BearerAdapter.BearerNetemToBearerHtbDTO(bearer_link.bearer_link_hbt)
+        # nest to a full bearer link with all children
+        return BearerLinkDTO(netem=netem, hbt=hbt)
 
     @staticmethod
     def BearerNetemToBearerNetemDTO(
         bearer_link_netem: BearerLinkNetem,
     ) -> BearerNetemDTO:
-        dto = BearerNetemDTO()
-        dto.delay = NetemAdapter.DelayDTO(
+        delay = NetemAdapter.DelayDTO(
             time=bearer_link_netem.delay_time,
             jitter=bearer_link_netem.delay_jitter,
             correlation=bearer_link_netem.delay_correlation,
         )
-        dto.loss = NetemAdapter.LossDTO(
+        loss = NetemAdapter.LossDTO(
             percentage=bearer_link_netem.loss_percentage,
             interval=bearer_link_netem.loss_interval,
             correlation=bearer_link_netem.loss_correlation,
         )
-        return BearerNetemDTO
+        return BearerNetemDTO(delay=delay, loss=loss)
 
     @staticmethod
     def BearerNetemToBearerHtbDTO(bearer_link_hbt: BearerLinkHBT) -> HBTDTO:
--- tests/unit/test_bearer_adapter.py
+++ tests/unit/test_bearer_adapter.py
@@ -3,9 +3,10 @@ from typing import List
 
 from pydantic import HttpUrl
 
-from app.adapters.bearer_adapter import BearerAdapter
-from app.dtos.bearer_dtos import BearerDTO
-from app.entities.models import Bearer
+from app.adapters.bearer_adapter import BearerAdapter, NetemAdapter
+from app.constants import LinkTypes
+from app.dtos.bearer_dtos import BearerDTO, BearerLinkDTO
+from app.entities.models import Bearer, BearerLink, BearerLinkHBT, BearerLinkNetem
 
 
 class TestBearerAdapter(unittest.TestCase):
@@ -15,6 +16,16 @@ class TestBearerAdapter(unittest.TestCase):
         self.title = "Test Bearer"
         self.description = "Test bearer description"
         self.img = "http://example.com/image.png"
+        self.delay = NetemAdapter.DelayDTO(
+            time=10,
+            jitter=5,
+            correlation=50,
+        )
+        self.loss = NetemAdapter.LossDTO(
+            percentage=5,
+            interval=100,
+            correlation=15,
+        )
 
     def test_BearerToBearerDetailsDTO(self):
         # arrange
@@ -103,6 +114,32 @@ class TestBearerAdapter(unittest.TestCase):
             title=self.title,
             img=self.img,
         )
+        bearer.bearer_links = []
+        uplink = self.create_bearer_link(
+            link_type_id=int(LinkTypes.UPLINK.value),
+            loss_percentage=5,
+            loss_interval=100,
+            loss_correlation=15,
+            delay_time=10,
+            delay_jitter=5,
+            delay_correlation=50,
+            ceil="25gbit",
+            rate="10kbit",
+        )
+        downlink = self.create_bearer_link(
+            link_type_id=int(LinkTypes.DOWNLINK.value),
+            loss_percentage=5,
+            loss_interval=100,
+            loss_correlation=15,
+            delay_time=10,
+            delay_jitter=5,
+            delay_correlation=50,
+            ceil="25gbit",
+            rate="10kbit",
+        )
+
+        bearer.bearer_links.append(uplink)
+        bearer.bearer_links.append(downlink)
 
         # act
         result = BearerAdapter.BearerToBearerDTO(bearer=bearer)
@@ -110,3 +147,110 @@ class TestBearerAdapter(unittest.TestCase):
         assert result.title == self.title
         assert result.img == HttpUrl(self.img)
         assert result.description == self.description
+        assert len(result.links) == 2
+        self.assert_bearer_link(result.links.get(LinkTypes.UPLINK.name), uplink)
+        self.assert_bearer_link(result.links.get(LinkTypes.DOWNLINK.name), downlink)
+
+    def create_bearer_link(
+        self,
+        link_type_id: int,
+        loss_percentage: int,
+        loss_interval: int,
+        loss_correlation: int,
+        delay_time: int,
+        delay_jitter: int,
+        delay_correlation: int,
+        ceil: str,
+        rate: str,
+    ) -> BearerLink:
+        bearer_link_netem = BearerLinkNetem()
+        bearer_link_netem.loss_percentage = loss_percentage
+        bearer_link_netem.loss_interval = loss_interval
+        bearer_link_netem.loss_correlation = loss_correlation
+        bearer_link_netem.delay_time = delay_time
+        bearer_link_netem.delay_jitter = delay_jitter
+        bearer_link_netem.delay_correlation = delay_correlation
+        bearer_link_hbt = BearerLinkHBT()
+        bearer_link_hbt.ceil = ceil
+        bearer_link_hbt.rate = rate
+        bearer_link = BearerLink()
+        bearer_link.link_type_id = link_type_id
+        bearer_link.bearer_link_netem = bearer_link_netem
+        bearer_link.bearer_link_hbt = bearer_link_hbt
+        return bearer_link
+
+    def assert_bearer_link(self, result: BearerLinkDTO, bearer_link: BearerLink):
+        assert (
+            result.netem.loss.percentage
+            == bearer_link.bearer_link_netem.loss_percentage
+        )
+        assert result.netem.loss.interval == bearer_link.bearer_link_netem.loss_interval
+        assert (
+            result.netem.loss.correlation
+            == bearer_link.bearer_link_netem.loss_correlation
+        )
+        assert result.netem.delay.time == bearer_link.bearer_link_netem.delay_time
+        assert result.netem.delay.jitter == bearer_link.bearer_link_netem.delay_jitter
+        assert (
+            result.netem.delay.correlation
+            == bearer_link.bearer_link_netem.delay_correlation
+        )
+        assert (
+            f"{result.hbt.ceil.value}{result.hbt.ceil.unit}"
+            == bearer_link.bearer_link_hbt.ceil
+        )
+        assert (
+            f"{result.hbt.rate.value}{result.hbt.rate.unit}"
+            == bearer_link.bearer_link_hbt.rate
+        )
+
+    def test_BearerLinkToBearerLinkDTO(self):
+        # arrange
+        bearer_link = self.create_bearer_link(
+            link_type_id=LinkTypes.DOWNLINK.value,
+            loss_percentage=5,
+            loss_interval=100,
+            loss_correlation=15,
+            delay_time=10,
+            delay_jitter=5,
+            delay_correlation=50,
+            ceil="25gbit",
+            rate="10kbit",
+        )
+
+        # act
+        result = BearerAdapter.BearerLinkToBearerLinkDTO(bearer_link)
+        # assert
+        self.assert_bearer_link(result, bearer_link)
+
+    def test_BearerNetemToBearerNetemDTO(self):
+        # arrange
+        bearer_link_netem = BearerLinkNetem()
+        bearer_link_netem.loss_percentage = 5
+        bearer_link_netem.loss_interval = 100
+        bearer_link_netem.loss_correlation = 15
+        bearer_link_netem.delay_time = 10
+        bearer_link_netem.delay_jitter = 5
+        bearer_link_netem.delay_correlation = 50
+        # act
+        result = BearerAdapter.BearerNetemToBearerNetemDTO(
+            bearer_link_netem=bearer_link_netem
+        )
+        # assert
+        assert result.loss.percentage == bearer_link_netem.loss_percentage
+        assert result.loss.interval == bearer_link_netem.loss_interval
+        assert result.loss.correlation == bearer_link_netem.loss_correlation
+        assert result.delay.time == bearer_link_netem.delay_time
+        assert result.delay.jitter == bearer_link_netem.delay_jitter
+        assert result.delay.correlation == bearer_link_netem.delay_correlation
+
+    def test_BearerNetemToBearerHtbDTO(self):
+        # arrange
+        bearer_link_hbt = BearerLinkHBT()
+        bearer_link_hbt.ceil = "25gbit"
+        bearer_link_hbt.rate = "10kbit"
+        # act
+        result = BearerAdapter.BearerNetemToBearerHtbDTO(bearer_link_hbt)
+        # assert
+        assert f"{result.ceil.value}{result.ceil.unit}" == bearer_link_hbt.ceil
+        assert f"{result.rate.value}{result.rate.unit}" == bearer_link_hbt.rate
