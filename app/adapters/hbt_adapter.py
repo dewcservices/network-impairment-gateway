ff1fcf16862fe3e204f89449c0d50ac5fcddfa5c
tnbozman <tnbozman@gmail.com>
2024-10-24 14:58:28 +1030

    feat: hbt adapter implemented and tested

--- app/adapters/hbt_adapter.py
+++ app/adapters/hbt_adapter.py
@@ -1,3 +1,5 @@
+import re
+
 from app.dtos.hbt_dtos import HBTDTO, HBTValueDTO
 from app.entities.models import BearerLinkHBT
 
@@ -6,10 +8,9 @@ class HBTAdapter:
 
     @staticmethod
     def BearerLinkHbtToDTO(hbt: BearerLinkHBT) -> HBTDTO:
-        dto = HBTDTO()
-        dto.ceil = HBTValueDTO(value=hbt.ceil, unit=hbt.ceil)
-        dto.rate = HBTValueDTO(value=hbt.rate, unit=hbt.rate)
-        return dto
+        ceil = HBTAdapter.ValueStrToHBTValueDTO(hbt.ceil)
+        rate = HBTAdapter.ValueStrToHBTValueDTO(hbt.rate)
+        return HBTDTO(ceil=ceil, rate=rate)
 
     @staticmethod
     def DTOToBearerLinkHbt(dto: HBTDTO) -> BearerLinkHBT:
@@ -17,3 +18,18 @@ class HBTAdapter:
         hbt.ceil = f"{dto.ceil.value}{dto.ceil.unit}"
         hbt.rate = f"{dto.rate.value}{dto.rate.unit}"
         return hbt
+
+    @staticmethod
+    def ValueStrToHBTValueDTO(input: str) -> HBTValueDTO:
+        # Use regular expression to capture value and unit
+        match = re.match(r"(\d+)(kbit|mbit|gbit)", input)
+
+        if not match:
+            raise ValueError(f"Invalid format for rate string: {input}")
+
+        # Extract value and unit from the regex match
+        value = int(match.group(1))
+        unit = match.group(2)
+
+        # Return an instance of HBTValueDTO
+        return HBTValueDTO(value=value, unit=unit)
