70a11509adbd4967b20a6db89e08e1235f5db644
Troy Boswell <troy.boswell@dewc.com>
2024-10-25 09:23:24 +0000

    feat: removed entrypoint tc settings

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -11,8 +11,5 @@ iptables -A FORWARD -i eth1 -o eth0 -j ACCEPT
 iptables -t nat -A POSTROUTING -o eth1 -j MASQUERADE
 iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 
-# Impair all traffic passing through as eth0 is passing to eth1
-tc qdisc add dev eth0 root netem delay 100ms loss 10%
-
 # Start fastapi app with uvicorn
 exec uvicorn app.main:app --host 0.0.0.0 --port 8000
