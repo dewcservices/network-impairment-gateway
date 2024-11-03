a3cd40e5aa9dbd9c84ad84c865de0ee73cb552af
Troy Boswell <troy.boswell@dewc.com>
2024-11-03 10:26:09 +0000

    fix: system state interface missing self

--- app/repositories/interfaces/isystem_state_repository.py
+++ app/repositories/interfaces/isystem_state_repository.py
@@ -5,9 +5,9 @@ from app.entities.models import SystemState
 
 class ISystemStateRepository(ABC):
     @abstractmethod
-    def get() -> SystemState:
+    def get(self) -> SystemState:
         pass
 
     @abstractmethod
-    def set(bearer_id: int, uplink_env_id: int, downlink_env_id: int) -> bool:
+    def set(self, bearer_id: int, uplink_env_id: int, downlink_env_id: int) -> bool:
         pass
